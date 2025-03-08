d709b6016eed5f373ed8e97d5855f946eb776e4f
Jeff4Dev <jeffmdev4@gmail.com>
2025-03-09 01:08:20 +0800

    add footer

---
+++ src/app/components/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer className="bg-white text-center text-gray-700 p-3 shadow-md">
+            © {new Date().getFullYear()} My Website. All rights reserved.
+        </footer>
+    );
+}
