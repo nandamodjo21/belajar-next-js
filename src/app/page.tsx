e76677249a057826e20e9a9c7947247c6e1be09c
Jeff4Dev <jeffmdev4@gmail.com>
2025-03-09 01:22:07 +0800

    update page app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,4 @@
-"use client";
-import Link from "next/link";
-import {useRouter} from "next/navigation";
-import ProductCard from "@/app/components/ProductCard";
-
-export default function Page(){
-
-
-  return (
-      <main>
-        <div><h1 className="text-3xl">Hello world</h1></div>
-        <div className="flex flex-col">
-          <Link href="/home">home</Link>
-          <Link href="/auth/login">Login</Link>
-          <ProductCard/>
-        </div>
-      </main>
-  )
+import { redirect } from 'next/navigation';
+export default function Home({}) {
+    redirect('/auth/login');
 }
